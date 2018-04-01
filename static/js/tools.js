65c52bbdce1bf14c8ad5c2f03c567d22c497eac2
amy liou <sourpluslot@gmail.com>
2018-04-01 08:02:16 +0800

    Add round rectangle library.

--- static/js/tools.js
+++ static/js/tools.js
@@ -1,7 +1,8 @@
 (function(){
     var TOOLS = {
         FIT_IMAGE_ON: ___FIT_IMAGE_ON,
-        UPDATE_COUNTER_ANIMATION: ___UPDATE_COUNTER_ANIMATION
+        UPDATE_COUNTER_ANIMATION: ___UPDATE_COUNTER_ANIMATION,
+        DRAW_ROUND_RECT: ___DRAW_ROUND_RECT
     };
 
 
@@ -51,6 +52,46 @@
         window.COUNTING_UPDATE(countInfo);
     }
 
+    // INFO: http://js-bits.blogspot.tw/2010/07/canvas-rounded-corner-rectangles.html
+    /**
+     * Draws a rounded rectangle using the current state of the canvas.
+     * If you omit the last three params, it will draw a rectangle
+     * outline with a 5 pixel border radius
+     * @param {CanvasRenderingContext2D} ctx
+     * @param {Number} x The top left x coordinate
+     * @param {Number} y The top left y coordinate
+     * @param {Number} width The width of the rectangle
+     * @param {Number} height The height of the rectangle
+     * @param {Number} radius The corner radius. Defaults to 5;
+     * @param {Boolean} fill Whether to fill the rectangle. Defaults to false.
+     * @param {Boolean} stroke Whether to stroke the rectangle. Defaults to true.
+     */
+    function ___DRAW_ROUND_RECT(ctx, x, y, width, height, radius, fill, stroke) {
+        if (typeof stroke == "undefined") {
+            stroke = true;
+        }
+        if (typeof radius === "undefined") {
+            radius = 5;
+        }
+        ctx.beginPath();
+        ctx.moveTo(x + radius, y);
+        ctx.lineTo(x + width - radius, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+        ctx.lineTo(x + width, y + height - radius);
+        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+        ctx.lineTo(x + radius, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+        ctx.lineTo(x, y + radius);
+        ctx.quadraticCurveTo(x, y, x + radius, y);
+        ctx.closePath();
+        if (stroke) {
+            ctx.stroke();
+        }
+        if (fill) {
+            ctx.fill();
+        }
+    }
+ 
 
 
     window.TOOLS = TOOLS;
